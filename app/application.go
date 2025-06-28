788d259aaf95db93913d6704ca77431518226006
wichupanj <mawnam@gmail.com>
2025-06-28 15:23:14 +0700

    update1

---
+++ app/application.go
@@ -0,0 +1,21 @@
+package app
+
+import "goapp/app/controllers"
+
+type Application struct {
+	Server *controllers.Server
+}
+
+func New() *Application {
+	server := controllers.New()
+
+	return &Application{
+		Server: server,
+	}
+}
+
+func (app *Application) Run() {
+	// Initialize the application components here
+	// For example, set up the database connection, start the server, etc.
+	app.Server.Run()
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "goapp/app"
+
+func main() {
+	app := app.New()
+	app.Run()
+}
